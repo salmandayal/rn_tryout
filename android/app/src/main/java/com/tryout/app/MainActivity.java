77172f770a56fa3cefecfbdd73155161de80ddbb
salmandayal <salmanrana888@gmail.com>
2023-01-18 16:41:06 +0500

    chore:Package rename

--- android/app/src/main/java/com/tryout/app/MainActivity.java
+++ android/app/src/main/java/com/tryout/app/MainActivity.java
@@ -1,4 +1,4 @@
-package com.tryout;
+package com.tryout.app;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
@@ -12,7 +12,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "tryout";
+    return "Tryout";
   }
 
   /**
--- android/app/src/main/java/com/tryout/app/newarchitecture/modules/MainApplicationTurboModuleManagerDelegate.java
+++ android/app/src/main/java/com/tryout/app/newarchitecture/modules/MainApplicationTurboModuleManagerDelegate.java
@@ -1,4 +1,4 @@
-package com.tryout.newarchitecture.modules;
+package com.tryout.app.newarchitecture.modules;
 
 import com.facebook.jni.HybridData;
 import com.facebook.react.ReactPackage;
